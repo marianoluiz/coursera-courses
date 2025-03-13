fc4b8fcf2f081f1b7cb1bdfb0d8dabc4a03b9abf
marianoluiz <kmarianu@gmail.com>
2025-03-13 22:42:58 +0800

    modified

--- react/fetching_api-react/src/Components/FetchData.jsx
+++ react/fetching_api-react/src/Components/FetchData.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import UseFetch from './UseFetch';
 import './FetchData.css'
 
--- react/fetching_api-react/src/Components/FetchYogaData.jsx
+++ react/fetching_api-react/src/Components/FetchYogaData.jsx
@@ -1,5 +1,4 @@
 
-import React from 'react'
 import UseFetch from './UseFetch'
 
 const FetchYogaData = () => {
